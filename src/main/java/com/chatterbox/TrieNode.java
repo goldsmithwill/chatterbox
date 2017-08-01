39c77f5d527165d6ba87d44bc9537df74f741174
goldsmithwill <goldsmith.will@gmail.com>
2017-08-01 14:39:31 -0400

    Boolean trie (fixed TrieNode)

--- src/main/java/com/chatterbox/TrieNode.java
+++ src/main/java/com/chatterbox/TrieNode.java
@@ -3,19 +3,12 @@ package com.chatterbox;
 public class TrieNode {
 	TrieNode[] arr;
 	boolean isEnd;
-	char letter;
 
 	// Constructor to initialize TrieNode objects
 	public TrieNode() {
 		// arr field to hold 26 TrieNodes
 		// 1 TrieNode for each letter in the alphabet
 		this.arr = new TrieNode[26];
-
-		// for loop to assign letter to each TrieNode in arr
-		for (int i = 0; i < 26; i++) {
-			char letter = (char) ('a' + i);
-			// this.arr[3];
-		}
 	}
 
 }
